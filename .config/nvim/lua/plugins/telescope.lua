6921394d8919a74540c524fe5e3211fad3b37fdb
ops <p.schiel@gmail.com>
2025-12-20 03:29:30 +0100

    sdf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,10 @@
 return {
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   {
     "nvim-telescope/telescope.nvim",
-    lazy = true,
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local telescope = require("telescope")
       local builtin = require("telescope.builtin")
-
-      -- Load fzf for speed
-      telescope.load_extension("fzf")
 
       -- Default setup with performance tweaks
       telescope.setup({
@@ -30,8 +25,9 @@ return {
       vim.keymap.set("n", "<leader>fa", builtin.find_files, { desc = "Find all files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>ff", builtin.git_files, { desc = "Find in git project" })
 
-      -- PHP-only files
+      -- PHP-only files keymap
       vim.keymap.set("n", "<leader>fp", function()
         builtin.find_files({
           prompt_title = "PHP Files",
@@ -39,18 +35,13 @@ return {
         })
       end, { desc = "Find PHP files" })
 
-      -- Grep only in PHP
+      -- Grep only in PHP keymap  
       vim.keymap.set("n", "<leader>gp", function()
         builtin.live_grep({
           prompt_title = "Grep in PHP",
           glob_pattern = "*.php",
         })
       end, { desc = "Grep in PHP files" })
-
-      -- Project search (fast, cached)
-      vim.keymap.set("n", "<leader>ff", function()
-        builtin.git_files({ show_untracked = true })
-      end, { desc = "Find in git project" })
     end,
   },
 }
